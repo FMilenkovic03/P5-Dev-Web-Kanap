bead56a145da76bdd9008e8d2905856477a98b91
FMilenkovic03 <fmilenkovic03@gmail.com>
2022-10-09 11:07:40 +0200

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -12,7 +12,10 @@ fetch('http://localhost:3000/api/products')
         produits.forEach(product => {
             let items = document.getElementById('items'); // item est select
             a = document.createElement('a');
-            article = document.createElement('article').appendChild(article.classList.add("carteProduit"));
+            article = document.createElement('article');
+            img = document.createElement('img');
+            h3 = document.createElement('h3').classList.add("productName");
+            p = document.createElement('p').classList.add("productDescription");
         })
         .catch((error) => {
             console.log(error);
