50c11b0574a3b5ba0908ec823c8558db26fdb8fe
FMilenkovic03 <fmilenkovic03@gmail.com>
2022-10-09 13:11:00 +0200

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -13,16 +13,24 @@ fetch('http://localhost:3000/api/products')
             //declaration et ajout de classe
             let items = document.getElementById('items');
             a = document.createElement('a');
+
             article = document.createElement('article');
             img = document.createElement('img');
             h3 = document.createElement('h3').classList.add("productName");
             p = document.createElement('p').classList.add("productDescription");
-            //assignation
+            //assignation enfant parent
             items.appendChild(a);
             a.appendChild(article);
             article.appendChild(img);
             article.appendChild(h3);
             article.appendChild(p);
+            //appel des elements
+            img.src = product.imageUrl;
+            img.alt = product.altTxt;
+            h3.innerHTML = product.name;
+            p.innerHTML = product.description;
+            console.log(product);
+
         })
     
     })
