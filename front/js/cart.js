7267e37a96fffeabfbede2cc58ee86e35c134af8
FMilenkovic03 <fmilenkovic03@gmail.com>
2022-10-19 11:51:36 +0200

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,4 +2,24 @@ let cartStorage = JSON.parse(localStorage.getItem("myCart"));
 
 //Création d'une fonction Panier pour appel aux éléments
 
-function displayCart() {}
+function displayCart() {
+    //let every div
+    //let every para
+    //attribute img too with input
+    //maybe fetch ?
+
+}
+
+function totalPrice(){
+
+}
+
+//modifier la quantité ?
+function alterQty(){}
+
+//validation and send form
+
+//error
+
+
+//fetch
