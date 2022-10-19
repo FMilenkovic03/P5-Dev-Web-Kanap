160b89f9e3389e0ce2c2960bf004d5f5f21759ab
FMilenkovic03 <fmilenkovic03@gmail.com>
2022-10-19 12:55:47 +0200

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,5 @@
 //requêter l'API pour les produits
 
-//maybe fetch in a function ? then call function ?
 
 fetch('http://localhost:3000/api/products')
     .then(function (res) {
@@ -18,23 +17,23 @@ fetch('http://localhost:3000/api/products')
             let Url = "./product.html?id=" + product._id;
             let article = document.createElement('article');
             let img = document.createElement('img');
-            let h3 = document.createElement('h3').classList.add("productName");
-            let p = document.createElement('p').classList.add("productDescription");
-            //assignation enfant parent
-            items.appendChild(a);
-            a.appendChild(article);
+            let h3 = document.createElement('h3');
+            let p = document.createElement('p');
+            
+            //lien a
+            a.href = Url;
+            //appel des elements
+            
+            img.src = product.imageUrl;
+            img.alt = product.altTxt;
+            h3.textContent = product.name;
+            p.textContent = product.description;
+            
             article.appendChild(img);
             article.appendChild(h3);
             article.appendChild(p);
-            //lien a
-            a.setAttribute('href', Url);
-            //appel des elements
-            a = product._id;
-            img.src = product.imageUrl;
-            img.alt = product.altTxt;
-            h3.innerHTML = product.name;
-            p.innerHTML = product.description;
-            
+            a.appendChild(article);
+            items.appendChild(a);
 
         })
 
@@ -42,3 +41,4 @@ fetch('http://localhost:3000/api/products')
     .catch((error) => {
         console.log(error);
     });
+    
