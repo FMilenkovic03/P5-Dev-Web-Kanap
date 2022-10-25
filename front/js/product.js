4f6401fb58be4ca57b7949ddd4e010c3b8410426
FMilenkovic03 <fmilenkovic03@gmail.com>
2022-10-25 10:57:21 +0200

    Update product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,41 +1,43 @@
 //fonction pour appeler un produit
 
-fetch("http://localhost:3000/api/products")
-    .then(function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then((product) => {
-        //appel elt
-        let imgCadre = document.querySelector(".item__img");
-        let nomProduit = document.querySelector("#title");
-        let prix = document.querySelector("#price");
-        let description = document.querySelector("#description");
-        let couleur = document.querySelector("#colors");
-        let imgProduit = document.createElement("img");
-        img.src = product.imageUrl;
-        img.alt = product.altTxt;
-        //product modification
-        nomProduit.innerText = `${product.name}`;
-        prix.innerText = `${product.price}`;
-        description.innerText = `${product.description}`;
-        //img append        
-        imgCadre.appendChild(imgProduit);
-        imgProduit.setAttribute("src", product.imageUrl);
-        imgProduit.setAttribute("alt", product.altTxt)
-        //color choosing
-        product.couleur.forEach(element => {
-            let option = document.createElement("option");
-            couleur.appendChild(option);
-            option.setAttribute("value", element);
-            option.innerText = element;
+function callProduct(){
+    fetch("http://localhost:3000/api/products")
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then((product) => {
+            //appel elt
+            let imgCadre = document.querySelector(".item__img");
+            let nomProduit = document.querySelector("#title");
+            let prix = document.querySelector("#price");
+            let description = document.querySelector("#description");
+            let couleur = document.querySelector("#colors");
+            let imgProduit = document.createElement("img");
+            img.src = product.imageUrl;
+            img.alt = product.altTxt;
+            //product modification
+            nomProduit.innerText = `${product.name}`;
+            prix.innerText = `${product.price}`;
+            description.innerText = `${product.description}`;
+            //img append        
+            imgCadre.appendChild(imgProduit);
+            imgProduit.setAttribute("src", product.imageUrl);
+            imgProduit.setAttribute("alt", product.altTxt)
+            //color choosing
+            product.couleur.forEach(element => {
+                let option = document.createElement("option");
+                couleur.appendChild(option);
+                option.setAttribute("value", element);
+                option.innerText = element;
+            });
+        })
+        .catch((error) => {
+            console.log(error);
         });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
+}
+callProduct();
     
 //add au panier
 
