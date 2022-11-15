eda8e5f92439e868bca9520df6bd089cbee31ce8
FMilenkovic03 <fmilenkovic03@gmail.com>
2022-11-15 10:07:37 +0100

    Update confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,6 @@
+const urlConfirmation = window.location.search;
+const urlSearchParams = new URLSearchParams(urlConfirmation);
 
+let order = document.querySelector("#orderId");
+order.innerHTML = urlSearchParams.get("orderId"); //récupère la clé orderId et l'insère dans le span
+localStorage.clear(); // vide le localStorage
