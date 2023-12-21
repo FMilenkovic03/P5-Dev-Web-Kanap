2d2194814a04d5f398499596deb54242448f59da
FMilenkovic03 <fmilenkovic03@gmail.com>
2023-12-21 22:16:03 +0100

    Refactor API request and dynamically create product elements

--- front/js/script.js
+++ front/js/script.js
@@ -1,55 +1,34 @@
-//requêter l'API pour les produits
+fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then(function(canapes) {
+        console.log(canapes)
 
+        canapes.forEach(function(canape) {
+            // Create new elements
+            let a = document.createElement('a');
+            let article = document.createElement('article');
+            let img = document.createElement('img');
+            let h3 = document.createElement('h3');
+            let p = document.createElement('p');
 
-fetch('http://localhost:3000/api/products')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+            // Set attributes and content
+            a.href = `./product.html?id=${canape._id}`;
+            img.src = canape.imageUrl;
+            img.alt = canape.altTxt;
+            h3.className = 'productName';
+            h3.textContent = canape.name;
+            p.className = 'productDescription';
+            p.textContent = canape.description;
 
-  
-fetch('http://localhost:3000/api/products')
-    .then(function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-
-    // Récupération des produits
-    .then(function (produits) {
-        produits.forEach(product => {
-            //declaration et ajout de classe
-            const items = document.getElementById('items');
-            const a = document.createElement('a');
-            const Url = "./product.html?id=" + product._id;
-            const article = document.createElement('article');
-            const img = document.createElement('img');
-            const h3 = document.createElement('h3');
-            const p = document.createElement('p');
-            
-            //lien a
-            a.href = Url;
-            //appel des elements
-            
-            img.src = product.imageUrl;
-            img.alt = product.altTxt;
-            h3.textContent = product.name;
-            p.textContent = product.description;
-            
+            // Append elements
             article.appendChild(img);
             article.appendChild(h3);
             article.appendChild(p);
             a.appendChild(article);
-            items.appendChild(a);
-
-        })
+            document.getElementById("items").appendChild(a);
+        });
 
     })
     .catch((error) => {
-        console.log(error);
-    });
-    
+        console.log(error)
+    }) 
