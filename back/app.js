093559c3f77a2f33d0e7f7b1121520ab50a89dee
FMilenkovic03 <fmilenkovic03@gmail.com>
2023-12-21 23:34:37 +0100

    Refactor app.js, product.js, and server.js

--- back/app.js
+++ back/app.js
@@ -12,10 +12,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/favicon.ico', (req, res) => {
-  res.status(204);
-});
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
 
